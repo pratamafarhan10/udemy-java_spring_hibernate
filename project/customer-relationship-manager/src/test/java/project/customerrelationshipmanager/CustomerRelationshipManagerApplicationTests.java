eb8280004321d8d34a3860f3aa64e81e3cda2426
pratamafarhan10 <pratamafarhan10@gmail.com>
2023-02-06 21:08:28 +0700

    section 28 done

---
+++ project/customer-relationship-manager/src/test/java/project/customerrelationshipmanager/CustomerRelationshipManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.customerrelationshipmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRelationshipManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
